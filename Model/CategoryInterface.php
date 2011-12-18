2fb9ebbe7bf3effb502900f39e8d98e75e2d2bf3
Edwin Ibarra <edwines.webmaster@gmail.com>
2011-12-18 14:07:27 -0500

    Creating the Model classes for the category table

---
+++ Model/CategoryInterface.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+ * This file is part of the Sonata project.
+ *
+ * (c) Sonata Project <https://github.com/sonata-project/SonataNewsBundle/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\NewsBundle\Model;
+
+interface CategoryInterface
+{
+    function setName($name);
+
+    /**
+     * Get name
+     *
+     * @return string $name
+     */
+    function getName();
+
+    /**
+     * Set enabled
+     *
+     * @param boolean $enabled
+     */
+    function setEnabled($enabled);
+
+    /**
+     * Get enabled
+     *
+     * @return boolean $enabled
+     */
+    function getEnabled();
+
+    /**
+     * Set slug
+     *
+     * @param integer $slug
+     */
+    function setSlug($slug);
+
+    /**
+     * Get slug
+     *
+     * @return integer $slug
+     */
+    function getSlug();
+    
+    /**
+     * Set description
+     * 
+     * @param string $description
+     */
+    function setDescription($description);
+    
+    /**
+     * Get description
+     * 
+     * @return string $description
+     */
+    function getDescription();
+    
+    /**
+     * Set count
+     * 
+     * @param integer $count
+     */
+    function setCount($count);
+    
+    /**
+     * Get count
+     * 
+     * @return integer $count
+     */
+    function getCount();
+    
+    /**
+     * Add posts
+     *
+     * @param Application\Sonata\NewsBundle\Model\Post $posts
+     */
+    function addPosts(PostInterface $posts);
+
+    /**
+     * Get posts
+     *
+     * @return Doctrine\Common\Collections\Collection $posts
+     */
+    function getPosts();
+}
