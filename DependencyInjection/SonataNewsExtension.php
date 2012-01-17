f6a34d7e13bddeaf6b1427d76d75e6aa21fbc618
Thomas Rabaix <thomas.rabaix@ekino.com>
2012-01-18 00:15:56 +0100

    Update skeleton to use the auto mapping feature from the Sonata EasyExtends Bundle

--- DependencyInjection/SonataNewsExtension.php
+++ DependencyInjection/SonataNewsExtension.php
@@ -17,7 +17,9 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\Definition;
-use \Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Reference;
+
+use Sonata\EasyExtendsBundle\Mapper\DoctrineCollector;
 
 /**
  * SonataNewsBundleExtension
@@ -26,6 +28,12 @@ use \Symfony\Component\DependencyInjection\Reference;
  */
 class SonataNewsExtension extends Extension
 {
+    /**
+     * @throws \InvalidArgumentException
+     * @param array $configs
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     * @return void
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $processor = new Processor();
@@ -67,5 +75,145 @@ class SonataNewsExtension extends Extension
             ->replaceArgument(5, array(
                 'notification' => $config['comment']['notification']
             ));
+
+
+        $this->registerDoctrineMapping($configs);
+    }
+
+    /**
+     * @param array $config
+     * @return void
+     */
+    public function registerDoctrineMapping(array $config)
+    {
+        $collector = DoctrineCollector::getInstance();
+
+        $collector->addAssociation('Application\\Sonata\\NewsBundle\\Entity\\Tag', 'mapManyToMany', array(
+            'fieldName'     => 'posts',
+            'targetEntity'  => 'Application\\Sonata\\NewsBundle\\Entity\\Post',
+            'cascade'       => array( ),
+            'mappedBy'      => 'tags',
+        ));
+
+        $collector->addAssociation('Application\\Sonata\\NewsBundle\\Entity\\Post', 'mapOneToMany', array(
+             'fieldName' => 'comments',
+             'targetEntity' => 'Application\\Sonata\\NewsBundle\\Entity\\Comment',
+             'cascade' =>
+             array(
+                 0 => 'remove',
+                 1 => 'persist',
+             ),
+             'mappedBy' => 'post',
+             'orphanRemoval' => true,
+             'orderBy' =>
+             array(
+                 'createdAt' => 'DESC',
+             ),
+        ));
+
+        $collector->addAssociation('Application\\Sonata\\NewsBundle\\Entity\\Post', 'mapOneToOne', array(
+            'fieldName' => 'image',
+            'targetEntity' => 'Application\\Sonata\\MediaBundle\\Entity\\Media',
+            'cascade' =>
+            array(
+                0 => 'remove',
+                1 => 'persist',
+                2 => 'refresh',
+                3 => 'merge',
+                4 => 'detach',
+            ),
+            'mappedBy' => NULL,
+            'inversedBy' => NULL,
+            'joinColumns' =>
+            array(
+                array(
+                    'name' => 'image_id',
+                    'referencedColumnName' => 'id',
+                ),
+            ),
+            'orphanRemoval' => false,
+        ));
+
+        $collector->addAssociation('Application\\Sonata\\NewsBundle\\Entity\\Post', 'mapOneToOne', array(
+             'fieldName' => 'author',
+             'targetEntity' => 'Application\\Sonata\\UserBundle\\Entity\\User',
+             'cascade' =>
+             array(
+                 1 => 'persist',
+             ),
+             'mappedBy' => NULL,
+             'inversedBy' => NULL,
+             'joinColumns' =>
+             array(
+                 array(
+                     'name' => 'author_id',
+                     'referencedColumnName' => 'id',
+                 ),
+             ),
+             'orphanRemoval' => false,
+        ));
+
+        $collector->addAssociation('Application\\Sonata\\NewsBundle\\Entity\\Post', 'mapOneToOne', array(
+             'fieldName' => 'category',
+             'targetEntity' => 'Application\\Sonata\\NewsBundle\\Entity\\Category',
+             'cascade' =>
+             array(
+                 1 => 'persist',
+             ),
+             'mappedBy' => NULL,
+             'inversedBy' => NULL,
+             'joinColumns' =>
+             array(
+                 array(
+                     'name' => 'category_id',
+                     'referencedColumnName' => 'id',
+                 ),
+             ),
+             'orphanRemoval' => false,
+        ));
+
+        $collector->addAssociation('Application\\Sonata\\NewsBundle\\Entity\\Post', 'mapManyToMany', array(
+            'fieldName' => 'tags',
+            'targetEntity' => 'Application\\Sonata\\NewsBundle\\Entity\\Tag',
+            'cascade' =>
+            array(
+                1 => 'persist',
+            ),
+            'joinTable' =>
+            array(
+                'name' => 'news__post_tag',
+                'joinColumns' =>
+                array(
+                    array(
+                        'name' => 'post_id',
+                        'referencedColumnName' => 'id',
+                    ),
+                ),
+                'inverseJoinColumns' =>
+                array(
+                    array(
+                        'name' => 'tag_id',
+                        'referencedColumnName' => 'id',
+                    ),
+                ),
+            ),
+        ));
+
+        $collector->addAssociation('Application\\Sonata\\NewsBundle\\Entity\\Comment', 'mapOneToOne', array(
+             'fieldName' => 'post',
+             'targetEntity' => 'Application\\Sonata\\NewsBundle\\Entity\\Post',
+             'cascade' => array(
+             ),
+             'mappedBy' => NULL,
+             'inversedBy' => NULL,
+             'joinColumns' =>
+             array(
+                 array(
+                     'name' => 'post_id',
+                     'referencedColumnName' => 'id',
+                 ),
+             ),
+             'orphanRemoval' => false,
+        ));
     }
 }
