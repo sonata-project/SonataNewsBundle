e2deadd8d2ee3a03222068f82a36057b37f7a832
Raphaël Benitte <benitteraphael@gmail.com>
2014-04-21 17:33:36 +0200

    Fix problem with invalid regular expression on API PostController 'tag' param

--- Controller/Api/PostController.php
+++ Controller/Api/PostController.php
@@ -98,7 +98,7 @@ class PostController
      * @QueryParam(name="enabled", requirements="0|1", nullable=true, strict=true, description="Enabled/Disabled posts filter")
      * @QueryParam(name="dateQuery", requirements=">|<|=", default=">", description="Date filter orientation (>, < or =)")
      * @QueryParam(name="dateValue", requirements="[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-2][0-9]:[0-5][0-9]:[0-5][0-9]([+-][0-9]{2}(:)?[0-9]{2})?", nullable=true, strict=true, description="Date filter value")
-     * @QueryParam(name="tag", requirements="\s", nullable=true, strict=true, description="Tag name filter")
+     * @QueryParam(name="tag", requirements="\S+", nullable=true, strict=true, description="Tag name filter")
      * @QueryParam(name="author", requirements="\s", nullable=true, strict=true, description="Author filter")
      * @QueryParam(name="mode", requirements="public|admin", default="public", description="'public' mode filters posts having enabled tags and author")
      *
