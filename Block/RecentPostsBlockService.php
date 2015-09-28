9808ba4595da355a6b59ccd8221c2f2f861e1d4f
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-09-28 21:34:51 +0200

    Fix deprecated OptionsResolver usage, fix #247

--- Block/RecentPostsBlockService.php
+++ Block/RecentPostsBlockService.php
@@ -20,7 +20,7 @@ use Sonata\CoreBundle\Model\ManagerInterface;
 use Sonata\CoreBundle\Validator\ErrorElement;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * @author     Thomas Rabaix <thomas.rabaix@sonata-project.org>
@@ -105,7 +105,7 @@ class RecentPostsBlockService extends BaseBlockService
     /**
      * {@inheritdoc}
      */
-    public function setDefaultSettings(OptionsResolverInterface $resolver)
+    public function configureSettings(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
             'number'     => 5,
