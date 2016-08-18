2d4502534b5eebaca5f31e8ab2597baba7d61916
Thomas Rabaix <thomas.rabaix@gmail.com>
2016-08-18 17:41:04 +0200

    Avoid form type name conflict
    The Form component (2.8 and upper) uses the FQDN as a key to store the object type.
    To avoid a conflict when registering the same type with different names, we create a child class for each name.

---
+++ Form/Type/ApiCommentType.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\NewsBundle\Form\Type;
+
+use Sonata\CoreBundle\Form\Type\BaseDoctrineORMSerializationType;
+
+/**
+ * @author  Thomas Rabaix <thomas.rabaix@gmail.com>
+ */
+class ApiCommentType extends BaseDoctrineORMSerializationType
+{
+}
