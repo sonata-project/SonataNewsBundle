50a5e2b42b7cea740703370de6fe4a73158caf2e
core23 <mail@core23.de>
2018-09-28 20:22:56 +0200

    Improved support for different db drivers

---
+++ tests/Model/NoDriverManagerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\NewsBundle\Tests\Model;
+
+use PHPUnit\Framework\TestCase;
+use Sonata\NewsBundle\Exception\NoDriverException;
+use Sonata\NewsBundle\Model\NoDriverManager;
+
+class NoDriverManagerTest extends TestCase
+{
+    /**
+     * @dataProvider mangerMethods
+     */
+    public function testException(string $method, array $arguments)
+    {
+        $this->expectException(NoDriverException::class);
+        \call_user_func_array([new NoDriverManager(), $method], $arguments);
+    }
+
+    public function mangerMethods()
+    {
+        return [
+            ['getClass', []],
+            ['findAll', []],
+            ['findBy', [[]]],
+            ['findOneBy', [[]]],
+            ['find', [1]],
+            ['create', []],
+            ['save', [null]],
+            ['delete', [null]],
+            ['getTableName', []],
+            ['getConnection', []],
+        ];
+    }
+}
