6d3e8410e0756d036624bc7e9285b84b8e00f396
FlorianKromer <f.kromer54@gmail.com>
2019-03-08 08:23:57 +0100

    fix : lint next major comment

--- src/DependencyInjection/SonataNewsExtension.php
+++ src/DependencyInjection/SonataNewsExtension.php
@@ -141,9 +141,8 @@ class SonataNewsExtension extends Extension
 
         foreach ($config['class'] as $type => $class) {
             if (!class_exists($class)) {
-                /**
-                 * NEXT_MAJOR: Uncomment this method
-                 *
+                /*
+                 * NEXT_MAJOR: 
                  * Throw an exception if the class is not defined
                  */
                 @trigger_error('The class '.$class.' is not defined or doesn\'t exist. This is tolerated now but will be forbidden in 4.0', E_USER_DEPRECATED);
