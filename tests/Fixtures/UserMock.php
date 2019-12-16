3c6b9081e5cae623c02f5c077ce52c9c211c27fa
core23 <mail@core23.de>
2019-12-16 17:43:05 +0100

    Remove tight coupling to user bundle

---
+++ tests/Fixtures/UserMock.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\NewsBundle\Tests\Fixtures;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserMock implements UserInterface
+{
+    public function getRoles()
+    {
+    }
+
+    public function getPassword()
+    {
+    }
+
+    public function getSalt()
+    {
+    }
+
+    public function getUsername()
+    {
+    }
+
+    public function eraseCredentials()
+    {
+    }
+}
