0ec504fcd8dff487f1a39f4ad7784e24ee5daf7e
Wojciech Błoszyk <wbloszyk@gmail.com>
2020-06-22 17:43:26 +0200

    Drop support for Symfony < 4.3

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -31,13 +31,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('sonata_news');
-
-        // Keep compatibility with symfony/config < 4.2
-        if (!method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->root('sonata_news');
-        } else {
-            $rootNode = $treeBuilder->getRootNode();
-        }
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
