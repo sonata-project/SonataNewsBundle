4877c32ab1c806ad7beb4d413a249bc5484573e3
Wojciech Błoszyk <wbloszyk@gmail.com>
2020-06-30 13:25:16 +0200

    Fix TwigStringExtensionCompilerPass priority

---
+++ tests/App/AppKernel.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\NewsBundle\Tests\App;
+
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use JMS\SerializerBundle\JMSSerializerBundle;
+use Knp\Bundle\MenuBundle\KnpMenuBundle;
+use Sonata\BlockBundle\SonataBlockBundle;
+use Sonata\ClassificationBundle\SonataClassificationBundle;
+use Sonata\Doctrine\Bridge\Symfony\Bundle\SonataDoctrineBundle;
+use Sonata\Form\Bridge\Symfony\SonataFormBundle;
+use Sonata\IntlBundle\SonataIntlBundle;
+use Sonata\MediaBundle\SonataMediaBundle;
+use Sonata\NewsBundle\SonataNewsBundle;
+use Sonata\Twig\Bridge\Symfony\SonataTwigBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\SecurityBundle\SecurityBundle;
+use Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+final class AppKernel extends Kernel
+{
+    use MicroKernelTrait;
+
+    public function __construct()
+    {
+        parent::__construct('test', false);
+    }
+
+    /**
+     * @return BundleInterface[]
+     */
+    public function registerBundles(): iterable
+    {
+        return [
+            new FrameworkBundle(),
+            new TwigBundle(),
+            new SecurityBundle(),
+            new DoctrineBundle(),
+            new KnpMenuBundle(),
+            new SonataBlockBundle(),
+            new SonataDoctrineBundle(),
+            new SonataFormBundle(),
+            new SonataTwigBundle(),
+            new SonataClassificationBundle(),
+            new SonataIntlBundle(),
+            new SonataMediaBundle(),
+            new SonataNewsBundle(),
+            new JMSSerializerBundle(),
+            new SwiftmailerBundle(),
+        ];
+    }
+
+    public function getCacheDir(): string
+    {
+        return $this->getBaseDir().'cache';
+    }
+
+    public function getLogDir(): string
+    {
+        return $this->getBaseDir().'log';
+    }
+
+    public function getProjectDir()
+    {
+        return __DIR__;
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder $routes): void
+    {
+        $routes->import($this->getProjectDir().'/config/routes.yaml');
+    }
+
+    protected function configureContainer(ContainerBuilder $containerBuilder, LoaderInterface $loader): void
+    {
+        $loader->load($this->getProjectDir().'/config/config.yaml');
+    }
+
+    private function getBaseDir(): string
+    {
+        return sys_get_temp_dir().'/sonata-news-bundle/var/';
+    }
+}
